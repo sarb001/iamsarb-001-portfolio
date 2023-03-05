034ae1e5f460b7d9042c1f3c0337e05fe2442347
SARB <mrsinghbusiness05@gmail.com>
2023-03-05 18:07:37 +0530

    vite + mui installed

---
+++ iamsarb-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from '@mui/material';
+
+function App() {
+
+  return (
+    <div className="App">
+        
+        <Button variant="contained"> Text </Button>
+    </div>
+  )
+}
+
+export default App
