79bc6cc2389a2eb20c2493d7e29471c1bfe4750b
SARB <mrsinghbusiness05@gmail.com>
2023-03-06 17:36:47 +0530

    Feature - Work UI Done

--- iamsarb-portfolio/src/Components/Work.jsx
+++ iamsarb-portfolio/src/Components/Work.jsx
@@ -1,10 +1,39 @@
+import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { Stack } from '@mui/system'
 import React from 'react'
 
 const Work = () => {
   return (
-    <div>
-    <h2> Work </h2>
+    <>
+    <div style = {{margin:'3% 4%'}}>
+          <div className = "work-section" style  = {{textAlign:'center'}}>
+             <span style = {{fontSize:"33px"}}> My Creative Portfolio Section  </span>
+          </div>
+
+            {/* cards */}
+                    <Stack direction = "row" spacing = {4}>
+                      
+                      <Card sx={{ maxWidth: 340,borderRadius:'15px' }}>
+                        <CardMedia  
+                          sx={{ height: 200 }}
+                          image="/src/assets/web-designer.png"
+                          title="green iguana"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h6" component="div">
+                              Tesla-Ecommerce 
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            A Full-Stack Website with UI 
+                            A Full-Stack Website with UI 
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                      
+                    </Stack>
+
     </div>
+    </>
   )
 }
 
